af3c191b05aa39644516817631f6b8abd7da8c76
Anthony Lesch <23031998+GutterBeats@users.noreply.github.com>
2022-09-12 08:42:30 -0700

    Created iOS project.

---
+++ The Watcher/The Watcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Watcher
+//
+//  Created by Anthony Lesch on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
